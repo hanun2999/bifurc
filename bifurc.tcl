e7ac07976031bbb5710b1a2b15ce8b27d9dcc731
Sam Bromley <sam@sambromley.com>
2012-04-26 22:58:47 -0230

    Change default values.

--- bifurc.tcl
+++ bifurc.tcl
@@ -1,11 +1,11 @@
 #!/usr/bin/env tclsh
 #
 
-set npoints 1024;
+set npoints 256;
 set niter 1000;
 
 set fout [open "out.dat" "w"];
-for {set r 0} {$r<4} {set r [expr {$r+0.001}]} {
+for {set r 2.5} {$r<4} {set r [expr {$r+0.002}]} {
 	set fp [open "|./bifurc $r $npoints $niter" r];
 	set vals [split [string trim [read $fp]] \n];
 	foreach v $vals {
